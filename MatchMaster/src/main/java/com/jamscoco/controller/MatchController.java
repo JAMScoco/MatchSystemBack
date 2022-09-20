ef7ad6a2315b0cb6bfc178f07d8ec029f03def40
WSDYNH <2872520012>
2022-09-20 22:41:17 +0800

    修改赛事

--- MatchMaster/src/main/java/com/jamscoco/controller/MatchController.java
+++ MatchMaster/src/main/java/com/jamscoco/controller/MatchController.java
@@ -29,7 +29,7 @@ import com.ruoyi.common.core.page.TableDataInfo;
  * @date 2022-09-20
  */
 @RestController
-@RequestMapping("/school/match")
+@RequestMapping("/match/history")
 public class MatchController extends BaseController
 {
     @Autowired
